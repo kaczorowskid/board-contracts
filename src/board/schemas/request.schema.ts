17ae3cc9453d30a6271004ce76696e58f9279ec7
Damian Kaczorowski <57078565+kaczorowskid@users.noreply.github.com>
2023-11-01 15:11:47 +0100

    feat: add validation rules (#5)
    * feat: add validation rules
    
    * feat: description max 500 chars

--- src/board/schemas/request.schema.ts
+++ src/board/schemas/request.schema.ts
@@ -5,33 +5,33 @@ import { prioLiteral, wholeBoardSchema } from "../common";
 export const getBoardRequestSchema = onlyIdSchema;
 export const updateBoardRequestSchema = wholeBoardSchema;
 export const createColumnRequestSchema = myzod.object({
-  title: myzod.string(),
+  title: myzod.string().min(3).max(200),
   board_id: idSchema,
 });
 export const removeColumnRequestSchema = onlyIdSchema;
 export const createTicketRequestSchema = myzod.object({
-  title: myzod.string(),
-  description: myzod.string(),
-  prio: myzod.literals("high", "medium", "low"),
+  title: myzod.string().min(3).max(200),
+  description: myzod.string().max(500),
+  prio: prioLiteral,
   column_id: idSchema,
   user_id: idSchema,
 });
 export const getBoardsWithPaginationRequestSchema = paginationSchema;
 export const createBoardRequestSchema = myzod.object({
-  title: myzod.string(),
-  description: myzod.string(),
+  title: myzod.string().min(3).max(200),
+  description: myzod.string().max(500),
   user_id: idSchema,
 });
 export const editBoardRequestSchema = myzod.object({
   id: idSchema,
-  title: myzod.string(),
-  description: myzod.string(),
+  title: myzod.string().min(3).max(200),
+  description: myzod.string().max(500),
 });
 export const removeBoardRequestSchema = onlyIdSchema;
 export const editTicketRequestSchema = myzod.object({
   id: idSchema,
-  title: myzod.string(),
-  description: myzod.string(),
+  title: myzod.string().min(3).max(200),
+  description: myzod.string().max(500),
   prio: prioLiteral,
 });
 export const removeTicketRequestSchema = onlyIdSchema;
@@ -39,7 +39,7 @@ export const getTicketRequestSchema = onlyIdSchema;
 export const getColumnRequestSchema = onlyIdSchema;
 export const editColumnRequestSchema = myzod.object({
   id: idSchema,
-  title: myzod.string(),
+  title: myzod.string().min(3).max(200),
 });
 export const confirmShareTokenRequestSchema = myzod.object({
   token: myzod.string(),
--- src/user/schemas/request.schema.ts
+++ src/user/schemas/request.schema.ts
@@ -1,28 +1,29 @@
 import myzod from "myzod";
 import { idSchema, onlyIdSchema } from "../../common";
+import validator from "validator";
 
 export const registerUserRequestSchema = myzod.object({
-  email: myzod.string(),
-  password: myzod.string(),
-  first_name: myzod.string().optional(),
-  last_name: myzod.string().optional(),
+  email: myzod.string().withPredicate(validator.isEmail, "expected email"),
+  password: myzod.string().min(3).max(40),
+  first_name: myzod.string().min(3).max(25).optional(),
+  last_name: myzod.string().min(3).max(25).optional(),
 });
 
 export const loginUserRequestSchema = myzod.object({
-  email: myzod.string(),
-  password: myzod.string(),
+  email: myzod.string().withPredicate(validator.isEmail, "expected email"),
+  password: myzod.string().min(3).max(40),
 });
 
 export const getUserRequestSchema = onlyIdSchema;
 
 export const resetPasswordUserRequestSchema = myzod.object({
-  email: myzod.string(),
+  email: myzod.string().withPredicate(validator.isEmail, "expected email"),
 });
 
 export const setNewPasswordUserRequestSchema = myzod.object({
   token: myzod.string().nullable(),
-  password: myzod.string(),
-  oldPassword: myzod.string(),
+  password: myzod.string().min(3).max(40),
+  oldPassword: myzod.string().min(3).max(40),
 });
 
 export const confirmAccountUserRequestSchema = myzod.object({
@@ -35,12 +36,12 @@ export const authorizeUserRequestSchema = myzod.object({
 
 export const updateUserRequestSchema = myzod.object({
   id: idSchema,
-  first_name: myzod.string(),
-  last_name: myzod.string(),
+  first_name: myzod.string().min(3).max(25),
+  last_name: myzod.string().min(3).max(25),
 });
 
 export const updatePasswordRequestSchema = myzod.object({
   id: idSchema,
-  old_password: myzod.string(),
-  new_password: myzod.string(),
+  old_password: myzod.string().min(3).max(40),
+  new_password: myzod.string().min(3).max(40),
 });
