99b23ddda8a436103cdc5cd5595eeca02a5382cf
sijab <kaczorowskid97@gmail.com>
2023-11-01 15:04:25 +0100

    feat: description max 500 chars

--- src/board/schemas/request.schema.ts
+++ src/board/schemas/request.schema.ts
@@ -11,7 +11,7 @@ export const createColumnRequestSchema = myzod.object({
 export const removeColumnRequestSchema = onlyIdSchema;
 export const createTicketRequestSchema = myzod.object({
   title: myzod.string().min(3).max(200),
-  description: myzod.string().max(200),
+  description: myzod.string().max(500),
   prio: prioLiteral,
   column_id: idSchema,
   user_id: idSchema,
@@ -19,19 +19,19 @@ export const createTicketRequestSchema = myzod.object({
 export const getBoardsWithPaginationRequestSchema = paginationSchema;
 export const createBoardRequestSchema = myzod.object({
   title: myzod.string().min(3).max(200),
-  description: myzod.string().max(200),
+  description: myzod.string().max(500),
   user_id: idSchema,
 });
 export const editBoardRequestSchema = myzod.object({
   id: idSchema,
   title: myzod.string().min(3).max(200),
-  description: myzod.string().max(200),
+  description: myzod.string().max(500),
 });
 export const removeBoardRequestSchema = onlyIdSchema;
 export const editTicketRequestSchema = myzod.object({
   id: idSchema,
   title: myzod.string().min(3).max(200),
-  description: myzod.string().max(200),
+  description: myzod.string().max(500),
   prio: prioLiteral,
 });
 export const removeTicketRequestSchema = onlyIdSchema;
